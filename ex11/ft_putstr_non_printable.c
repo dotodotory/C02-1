427c3a5b912160427221a65f5f5b6f0c5c1bd224
Jiyoung Kim <jiykim@c8r2s14.42seoul.kr>
2020-10-26 00:56:57 +0900

    mod 10 11

--- ex11/ft_putstr_non_printable.c
+++ ex11/ft_putstr_non_printable.c
@@ -6,7 +6,7 @@
 /*   By: jiykim <jiykim@student.42seoul.kr>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/10/21 20:10:38 by jiykim            #+#    #+#             */
-/*   Updated: 2020/10/25 23:48:14 by jiykim           ###   ########.fr       */
+/*   Updated: 2020/10/26 00:54:28 by jiykim           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,18 +15,16 @@
 char	*ft_hexa(char d)
 {
 	char hexa[3];
+	char h[16] = "123456789abcdef";
 
 	hexa[0] = '\\';
-	hexa[1] = "123456789abcdef"[d / 16];
-	hexa[2] = "123456789abcdef"[d % 16];
+	hexa[1] = h[d / 16];
+	hexa[2] = h[d % 16];
 	return (hexa);
 }
 
 void	ft_putstr_non_printable(char *str);
 {
-	char a;
-	char b;
-
 	while (str[i] != '\0')
 	{
 		if (str[i] < 32 || str[i] > 127)
